da4c268b8899da6664301a4a6ec6a9243e512630
KattiaBodden <kattiabodden@gmail.com>
2021-12-04 23:18:30 -0600

    Agregando el proyecto al repositorio

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOM from 'react-dom'; 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App />, //lo que vamos a renderizar
+  document.getElementById('root') // donde se va a renderizar
 );
 
 // If you want to start measuring performance in your app, pass a function
